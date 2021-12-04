002634e80eefa546aeeee40b533d9820bca6904d
luxuncang <2635886314@qq.com>
2021-12-05 01:18:51 +0800

    #3

--- src/SimilarNeuron/exception.py
+++ src/SimilarNeuron/exception.py
@@ -1,18 +1,38 @@
 class SimilarNeuronException(Exception):
-    
+
     def __init__(self, name: str, reason: str) -> None:
         self.name = name
         self.reason = reason
-    
+
     def __str__(self) -> str:
         return f"{self.name}: {self.reason}"
 
+
 class AnnotationEmpty(SimilarNeuronException):
-    
-    def __init__(self, name: str = 'AnnotationEmpty', reason: str = 'The function signature has no comments.') -> None:
+
+    def __init__(
+        self, 
+        name: str = 'AnnotationEmpty', 
+        reason: str = 'The function signature has no comments.'
+        ) -> None:
         super().__init__(name, reason)
+
 
 class TransformError(SimilarNeuronException):
 
-    def __init__(self, name: str = 'TransformError', reason: str = 'Type conversion error, view conversion object.') -> None:
+    def __init__(
+        self, 
+        name: str = 'TransformError', 
+        reason: str = 'Type conversion error, look up `transform` object.'
+        ) -> None:
+        super().__init__(name, reason)
+
+
+class SwitchEmptyError(SimilarNeuronException):
+
+    def __init__(
+        self, 
+        name: str = 'SwitchEmptyError', 
+        reason: str = 'This `Switch` object not in Agreement.'
+        ) -> None:
         super().__init__(name, reason)
